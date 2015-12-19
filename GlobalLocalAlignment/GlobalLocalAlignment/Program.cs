50faa3c2d0accd512640a5c79e4b1fe49f5003bc
tired_papasmurf <akrygosk@ramapo.edu>
2015-12-19 14:19:41 -0500

    First Commit

---
+++ GlobalLocalAlignment/GlobalLocalAlignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalLocalAlignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ah god I don't know what I'm doing!");
+        }
+    }
+}

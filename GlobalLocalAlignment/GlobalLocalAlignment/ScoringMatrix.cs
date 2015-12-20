05cd0c86ab14278f8fd0db7b057fa44fce0257e2
Mingzhao Liu <mliu2@ramapo.edu>
2015-12-20 14:04:30 -0500

    Implemented Local Alignment
    Local alignment checks at each cell, if the score would be negative, set
    it to 0

--- GlobalLocalAlignment/GlobalLocalAlignment/ScoringMatrix.cs
+++ GlobalLocalAlignment/GlobalLocalAlignment/ScoringMatrix.cs
@@ -115,6 +115,65 @@ namespace GlobalLocalAlignment
             throw new ScoringClassException("Somehow Andrew bollocksed the code");
 
         }
+        /*
+        * Parameters: the row followed by column of cell that we want to calculate
+        * Returns: The direction attained (top|left|diagnol), or -1 if it already has a number or if invalid input
+        * Side Effect: inputs the calculated number into that cell in theMatrix
+        * 
+        * Note: (top|left|diagnol) == bits, agreed that 101 implies number was attained from top and diagnol, 1 means attained only from diagnol, etc
+        * 
+        * TODO: open vs. continue indel logic
+        */
+        public int calcCellLocal(int row, int col)
+        {
+            if (row == 0 || col == 0) { return -1; } //these should be filled in by default, also will throw error if continue
+
+            int diagnol = this.theMatrix[row - 1, col - 1] + ((this.top[row] == this.side[col]) ? (this.scoringSystem[0]) : (this.scoringSystem[1]));
+            int top = this.theMatrix[row - 1, col] + (this.scoringSystem[2]); //TODO HERE: going with indelOpen for now
+            int side = this.theMatrix[row, col - 1] + (this.scoringSystem[2]); //TODO HERE: going with indelOpen for now
+            int max = -9999999;
+            //Smith Waterman local alignments sets cells that would go negative to 0
+            if (diagnol < 0 && top < 0 && side < 0)
+            {
+                max = 0;
+            }
+            else
+            {
+                if (diagnol >= top && diagnol >= side) { max = diagnol; }
+                else if (top >= diagnol && top >= side) { max = top; }
+                else if (side >= diagnol && side >= top) { max = side; }
+                else
+                {
+                    Console.WriteLine("I have no idea what happened, this is impossible...");
+                    throw new ScoringClassException("Somehow Ming bollocksed the code");
+                }
+            }
+
+            if (diagnol > top)
+            {
+                if (diagnol > side) { this.theMatrix[row, col] = max; return 1; }
+                else if (diagnol == side) { this.theMatrix[row, col] = max; return 11; }
+                this.theMatrix[row, col] = side;//if neither of those than side is bigger than diagnol, also not equal to or smaller than top
+                return 10;
+            }
+            else if (diagnol < top)
+            {
+                if (side < top) { this.theMatrix[row, col] = max; return 100; }
+                else if (side == top) { this.theMatrix[row, col] = max; return 110; }
+                this.theMatrix[row, col] = side;//if neither of those than side is bigger than diagnol, also not equal to or smaller than top
+                return 10;
+            }
+            else if (diagnol == top)
+            {
+                if (diagnol == side) { this.theMatrix[row, col] = max; return 111; }
+                else if (diagnol > side) { this.theMatrix[row, col] = max; return 101; }
+                this.theMatrix[row, col] = side;//if neither of those than side is bigger than diagnol, also not equal to or smaller than top
+                return 10;
+            }
+            Console.WriteLine("I have no idea what happened, this is impossible...");
+            throw new ScoringClassException("Somehow Ming bollocksed the code");
+
+        }
 
         public int getPos(int row, int col) { return this.theMatrix[row, col]; }
 
